f0ef2c011fb79324459756de43c0f01da38bb2fb
Zoe <zmb6893@g.rit.edu>
2024-09-21 18:34:11 -0400

    Added in the projectCards

---
+++ src/sections/projectCards.js
@@ -0,0 +1,80 @@
+class ProjectCards extends HTMLElement {
+	constructor () {
+		super()
+	}
+
+	projects = [
+		{
+			title: '3DSea',
+			description: 'A vast library of 3D web components, created using THREE,js',
+			link: 'https://github.com/zmb6893',
+			linkTitle: 'Source Code',
+			image: '/assets/images/background/oak-flower.jpg',
+			alt: '3DSea Component Library Logo'
+		},
+		{
+			title: 'ApolloLink',
+			description: 'A web application that prempetively detects signs of disease from the users smart watch',
+			link: 'https://github.com/zmb6893',
+			linkTitle: 'Source Code',
+			image: '/assets/images/background/oak-flower.jpg',
+			alt: '3DSea Component Library Logo'
+		},
+		{
+			title: 'BrickVibes',
+			description: 'Detects emotional state based on music trends of a user',
+			link: 'https://github.com/zmb6893',
+			linkTitle: 'Source Code',
+			image: '/assets/images/background/oak-flower.jpg',
+			alt: '3DSea Component Library Logo'
+		},
+	]
+
+	connectedCallback() {
+		this.attachShadow({mode: 'open'})
+
+		// Import bootstrap css
+		const bsCss = document.createElement('link');
+		bsCss.setAttribute('rel', 'stylesheet');
+		bsCss.setAttribute('href', '/assets/bootstrap-5.3.3-dist/css/bootstrap.min.css');
+		// import custom css
+		const themeCss = document.createElement('link');
+		themeCss.setAttribute('rel', 'stylesheet');
+		themeCss.setAttribute('href', '/assets/css/theme-formatting.css');
+
+
+		const projectCards = this.createProjectCards();
+		this.shadowRoot.innerHTML = `
+		
+			<div class='row gx-5 gy-5'>
+				${projectCards}
+			</div>
+		`
+
+
+		// Add everything to the document
+		this.shadowRoot.appendChild(bsCss);
+		this.shadowRoot.appendChild(themeCss);
+	}
+
+	createProjectCards() {
+		let projectCards = ''
+		this.projects.forEach ((project, index) => {
+			projectCards += ((index + 1) % 4) === 0 ? `</div><div class='row gx-5 gy-5' style='padding-top: 2rem'>` : ''
+			projectCards += `
+				<div class='col-4'>
+					<custom-card 
+						title="${project.title}" 
+						description="${project.description}" 
+						image="${project.image}"
+						linkTitle="${project.linkTitle}"
+						link="${project.link}"
+					></custom-card>
+				</div>
+			`
+		})
+		return projectCards;
+	}
+}
+
+window.customElements.define('project-card-section', ProjectCards)
